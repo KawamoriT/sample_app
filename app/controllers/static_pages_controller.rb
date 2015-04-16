267204e7a6539c976362c16d3f419ac1340b8c10
Takumi Kawamori <takumi@kawamori.jp>
2015-04-16 17:43:59 +0900

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,11 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def help
+  def home
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def about
